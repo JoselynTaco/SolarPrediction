f3ecb9ea41a80bc05b5974e1b7f0071c2d719a99
JoselynTaco <77494645+JoselynTaco@users.noreply.github.com>
2021-09-14 03:34:11 -0500

    Update postPython.py

--- postPython.py
+++ postPython.py
@@ -1,24 +1,24 @@
 import pickle
 import numpy as np
-from sklearn import preprocessing
 from flask import Flask, jsonify, request, json
 
 app = Flask(__name__)
 
 # {"Temperature":0,"Pressure":0,"Humidity":0,"WindDirection(Degrees)":0,"Speed":0,"mes":0,"hora":0,"diferencia":0}
 
-@app.route('/')
-def holamundo():
+@app.route('/hola')
+def hola():
     return 'Aqui'
 
 @app.route('/prediction',methods=['POST'])
 def prediction():
     content = request.get_json()
+
     datos = np.array([[content['Temperature'],content['Pressure'],content['Humidity'],content['WindDirection'],content['Speed'],content['mes'],content['hora'],content['diferencia']]])
-    datos = preprocessing.scale(datos)
     modelo = pickle.load(open("modelRF.h5", 'rb'))
 
     result = str(modelo.predict(datos)[0])
+    print(datos)
     d = {"result": result}
     response = app.response_class(
         response=json.dumps(d),
